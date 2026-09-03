277faf6a89021941d3ad11d888cdc9000134492f
jan-cardell <janpetercardell@outlook.de>
2026-09-03 22:11:35 +0200

    final watchlist functionality

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const exploringDiv = document.getElementById('exploring')
 const searchBtn = document.getElementById('search-btn')
 const searchInput = document.getElementById('search-input')
-let watchlist = {}
+let watchlist = JSON.parse( localStorage.getItem("watchlist") )
 
 searchBtn.addEventListener('click', getMovies)
 
@@ -22,19 +22,23 @@ document.addEventListener('click', function(e){
                             <div id='movie-subhead'>
                                 <p id='movie-runtime'>${data.Runtime}</p>
                                 <p id='movie-genre'>${data.Genre}</p>
-                                <div id='add-watchlist-btn' data-add='${data.imdbID}'>
-                                    <i id='add-plus' class="fa-solid fa-circle-plus" data-add='${data.imdbID}'></i>
-                                    <p data-add='${data.imdbID}'>Watchlist</p>
+                                <div id='add-watchlist-btn' data-remove='${data.imdbID}'>
+                                    <i id='add-plus' class="fa-solid fa-circle-minus" data-remove='${data.imdbID}'></i>
+                                    <p data-remove='${data.imdbID}'>Remove</p>
                                 </div>
                             </div>
                             <p id='movie-plot'>${data.Plot}</p>
                         </div>
                     </div>
                     `}
-                
-                console.log(watchlist)
                 renderLocalStorage()
             })
+    }
+})
+
+searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        searchBtn.click()
     }
 })
 
--- watchlist.js
+++ watchlist.js
@@ -1,15 +1,33 @@
 const watchlistDiv = document.getElementById('watchlist')
 const moviesFromLocalStorage = JSON.parse( localStorage.getItem("watchlist") )
- 
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.remove){
+        delete moviesFromLocalStorage[e.target.dataset.remove]
+        localStorage.setItem('watchlist', JSON.stringify(watchlist))
+        renderWatchlist()
+        checkForEmpty()
+    }
+})
 
 function renderWatchlist() {
     let watchlistHtml = ''
     for (const imdbID in moviesFromLocalStorage) {
         watchlistHtml += moviesFromLocalStorage[imdbID].body
     }
-    console.log(watchlistHtml)
     watchlistDiv.innerHTML = watchlistHtml
 }
 
+function checkForEmpty() {
+    if (Object.keys(moviesFromLocalStorage).length === 0){
+    watchlistDiv.innerHTML=
+    `            <p>Your watchlist is looking a little empty...</p>
+    <a id="add-movies" href="index.html">
+        <i class="fa-solid fa-circle-plus"></i>
+        <p>Let's add some movies!</p>
+    </a>`
+}}
+
 renderWatchlist()
+checkForEmpty()
 
